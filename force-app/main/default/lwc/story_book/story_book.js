f38f8be1f298be5ffb3120e83fcf9816cc967343
Paul Roth <paulroth3d@gmail.com>
2020-01-29 17:47:03 -0600

    PARTIAL: support sizing of the story sandbox width and height

--- force-app/main/default/lwc/story_book/story_book.js
+++ force-app/main/default/lwc/story_book/story_book.js
@@ -29,30 +29,61 @@ export default class Story_book extends LightningElement {
   @api description;
 
   /**
-   * Size of the storybook story
-   * @type {String} - [SMALL|MEDIUM|LARGE]
+   * Width of the storybook story
+   * @type {String} - [SMALL|MEDIUM|LARGE|:css]
   */
-  @api size;
+  @api width;
+
+  /**
+   * Height of the storybook story
+   * @type {String} - [String:css]
+  */
+  @api height;
 
   /** whether to include a border for the component */
   @api border;
 
   get storyBookClasses() {
-    let sizeClass = '';
-    if (this.size) {
-      switch(this.size.toUpperCase()) {
-        case SIZE_SMALL: sizeClass = 'small'; break;
-        case SIZE_MEDIUM: sizeClass = 'medium'; break;
-        case SIZE_LARGE: sizeClass = 'large'; break;
-        default: break;
-      }
-    }
-
     let borderedClass = '';
-    if (this.border) {
+    if ((''+this.border).toUpperCase()==='TRUE') {
       borderedClass = 'bordered';
     }
-    return `slds-box slds-theme_default storybook ${sizeClass} ${borderedClass}`;
+    return `slds-box slds-theme_default storybook ${borderedClass}`;
+  }
+
+  get sandboxStyle() {
+    let styleCSS = '';
+    // styleCSS += ` width: ${this.width};`;
+    if (this.width) {
+      switch(this.width.toUpperCase()) {
+        case SIZE_SMALL: styleCSS += 'width: 30em;'; break;
+        case SIZE_MEDIUM: styleCSS += 'width: 48em;'; break;
+        case SIZE_LARGE: styleCSS += 'width: 64em;'; break;
+        default: styleCSS += `width: ${this.width};`;
+      }
+    }
+    console.log('width:' + this.width);
+    console.log('styleCSS:' + styleCSS);
+    styleCSS += ' height: 300px;';
+    return styleCSS;
+  }
+
+  get sandboxStyle2() {
+    let styleCSS = '';
+    if (this.width) {
+      switch(this.width.toUpperCase()) {
+        case SIZE_SMALL: styleCSS += 'width: 30em;'; break;
+        case SIZE_MEDIUM: styleCSS += 'width: 48em;'; break;
+        case SIZE_LARGE: styleCSS += 'width: 64em;'; break;
+        default: styleCSS += `width: ${this.width};`;
+      }
+    }
+    if (this.height) {
+      switch(this.height.toUpperCase()) {
+        default: styleCSS += `height: ${this.height};`;
+      }
+    }
+    return styleCSS;
   }
 
   /** 
--- force-app/main/default/lwc/story_exampleComplex/story_exampleComplex.js
+++ force-app/main/default/lwc/story_exampleComplex/story_exampleComplex.js
@@ -20,13 +20,18 @@ export default class Story_exampleComplex extends LightningElement {
   @api allScenes = [
     new Scene('Wide Scene', {
       description: 'Wide component with a message',
-      size: 'wide',
+      width: 'wide',
+      height: '300px',
       message: `
-I am the very model of a modern Major-General
-I've information vegetable, animal, and mineral
-I know the kings of England, and I quote the fights
-Historical
-From Marathon to Waterloo, in order categorical`
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus vestibulum mattis ullamcorper velit sed. Tristique nulla aliquet enim tortor. Massa tincidunt dui ut ornare lectus sit amet est. Senectus et netus et malesuada fames. Pharetra et ultrices neque ornare aenean. Eget felis eget nunc lobortis mattis aliquam faucibus. Arcu bibendum at varius vel pharetra vel. Quis risus sed vulputate odio ut enim blandit volutpat. Vel eros donec ac odio tempor orci. Facilisis volutpat est velit egestas dui. Viverra nibh cras pulvinar mattis nunc sed blandit libero. Eu feugiat pretium nibh ipsum consequat nisl. Purus viverra accumsan in nisl nisi scelerisque.
+
+Eu lobortis elementum nibh tellus molestie nunc. Dapibus ultrices in iaculis nunc sed. Nam at lectus urna duis convallis convallis tellus id interdum. Arcu risus quis varius quam quisque id diam. Egestas dui id ornare arcu odio ut sem nulla pharetra. Diam ut venenatis tellus in metus vulputate eu scelerisque. Quis viverra nibh cras pulvinar mattis. Eu tincidunt tortor aliquam nulla facilisi. Feugiat nibh sed pulvinar proin gravida hendrerit lectus. Faucibus a pellentesque sit amet. Varius sit amet mattis vulputate. Bibendum arcu vitae elementum curabitur vitae nunc. Dictum fusce ut placerat orci nulla pellentesque. Est lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet cursus sit amet dictum. Eu ultrices vitae auctor eu augue ut lectus arcu. Sodales neque sodales ut etiam sit amet nisl. Lacus sed viverra tellus in hac habitasse platea dictumst. Eu feugiat pretium nibh ipsum consequat. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu.
+
+Morbi tempus iaculis urna id volutpat lacus laoreet. Diam quis enim lobortis scelerisque fermentum dui. Ut consequat semper viverra nam libero justo laoreet sit amet. Egestas congue quisque egestas diam in arcu cursus euismod quis. Sit amet nulla facilisi morbi tempus iaculis urna id. Fermentum leo vel orci porta non pulvinar. Enim tortor at auctor urna nunc id cursus metus aliquam. Morbi non arcu risus quis. Vel pretium lectus quam id leo in vitae turpis massa. Aliquam id diam maecenas ultricies mi eget mauris pharetra et. Nec tincidunt praesent semper feugiat nibh sed.
+
+Tempus iaculis urna id volutpat lacus laoreet non. Neque convallis a cras semper auctor neque vitae tempus. Pharetra massa massa ultricies mi quis hendrerit. Scelerisque fermentum dui faucibus in ornare quam. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Suscipit adipiscing bibendum est ultricies. Maecenas accumsan lacus vel facilisis. Fusce id velit ut tortor pretium. Arcu non sodales neque sodales ut etiam sit amet nisl. Gravida neque convallis a cras semper auctor. Sed vulputate odio ut enim blandit volutpat maecenas. Vel facilisis volutpat est velit egestas dui id ornare arcu. Interdum varius sit amet mattis vulputate.
+
+Donec ultrices tincidunt arcu non sodales neque sodales ut. Aliquam eleifend mi in nulla posuere sollicitudin aliquam. Vulputate enim nulla aliquet porttitor. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Cursus euismod quis viverra nibh cras pulvinar. Magna fringilla urna porttitor rhoncus dolor purus. Enim tortor at auctor urna nunc. Massa tempor nec feugiat nisl pretium fusce id velit. Rutrum tellus pellentesque eu tincidunt. In nibh mauris cursus mattis molestie a iaculis. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Curabitur gravida arcu ac tortor dignissim convallis. Nibh praesent tristique magna sit amet purus gravida quis. Ac turpis egestas integer eget aliquet nibh praesent tristique. Nunc eget lorem dolor sed viverra ipsum nunc.`
     }),
     new Scene('Empty Scene', {
       description: 'Description for the scene',
